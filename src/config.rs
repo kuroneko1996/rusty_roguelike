1bd36a8f65b44892c0db457d381358ab6e041766
kuroneko1996 <kuroneko1996@users.noreply.github.com>
2016-10-29 00:23:57 +0300

    add rect methods

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,10 @@ pub const SCREEN_HEIGHT: i32 = 50;
 pub const MAP_WIDTH: i32 = 80;
 pub const MAP_HEIGHT: i32 = 45;
 
+pub const ROOM_MAX_SIZE: i32 = 10;
+pub const ROOM_MIN_SIZE: i32 = 6;
+pub const ROOM_ROOMS: i32 = 30;
+
 pub const LIMIT_FPS: i32 = 20;
 
 pub const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
